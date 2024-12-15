4cec37071753851c6f6c62c97f0a6b9ce4703b6e
flux <49762827+0xflux@users.noreply.github.com>
2024-12-15 18:40:59 +0000

    Keep track of processes is userland

--- um_engine/src/core/core.rs
+++ um_engine/src/core/core.rs
@@ -1,6 +1,10 @@
 use std::{sync::Arc, thread::sleep, time::Duration};
 
-use crate::engine::UmEngine;
+use shared_no_std::ioctl::DriverMessages;
+
+use crate::{engine::UmEngine, utils::log::{Log, LogLevel}};
+
+use super::process_monitor::ProcessMonitor;
 
 pub struct Core {
     driver_poll_rate: u64,
@@ -11,37 +15,68 @@ impl Core {
     /// Starts the core of the usermode engine; kicking off the frequent polling of the 
     pub async fn start_core(engine: Arc<UmEngine>) -> ! {
 
+        println!("Core starting");
+
         // create a local self contained instance of Core, as we don't need to instantiate 
         // the core outside of this entry function
         let core = Core {
-            driver_poll_rate: 60,
+            driver_poll_rate: 500,
         };
+
+        let mut processes = ProcessMonitor::new();
+
+        let logger = Log::new();
 
         //
         // Enter the polling & decision making loop, this here is the core / engine of the usermode engine.
         //
         loop {
             // contact the driver and get any messages from the kernel 
-            let driver_response = engine.driver_manager.lock().unwrap().ioctl_get_driver_messages();
-                if driver_response.is_some() {
-                    // println!("x: {:?}", driver_response);
-                    // todo
-
-                    // cache messages 
-                    // add process creations to a hashmap (ProcessMonitor struct)
-
-                    /*
-                        todo long term: 
-                            - thread creation 
-                            - handle requests
-                            - change of handle type (e.g. trying to evade detection)
-                            - is the process doing bad things itself (allocating foreign mem)
-                            
-                        ^ to the abv hashmap
-                    */
+            let driver_response = {
+                let mut mtx = engine.driver_manager.lock().unwrap();
+                mtx.ioctl_get_driver_messages()
+            };
+            
+            //
+            // If we have new message(s) from the driver, process them in userland as appropriate 
+            //
+            if driver_response.is_some() {
+                // first deal with process terminations to prevent trying to add to an old process id if there is a duplicate
+                let driver_messages = driver_response.unwrap();
+                let process_terminations = driver_messages.process_terminations;
+                if !process_terminations.is_empty() {
+                    for t in process_terminations {
+                        processes.remove_process(t.pid);
+                    }
                 }
 
-                sleep(Duration::from_millis(core.driver_poll_rate));
+                // add a new process to the running process hashmap
+                let process_creations = driver_messages.process_creations;
+                if !process_creations.is_empty() {
+                    for p in process_creations {
+                        if processes.insert(&p).is_err() {
+                            logger.log(LogLevel::Error, &format!("Failed to add new process to live processes. Process: {:?}", p));
+                        }
+                    }
+                }
+
+                println!("{:?}", processes);
+
+                // cache messages 
+                // add process creations to a hashmap (ProcessMonitor struct)
+
+                /*
+                    todo long term: 
+                        - thread creation 
+                        - handle requests
+                        - change of handle type (e.g. trying to evade detection)
+                        - is the process doing bad things itself (allocating foreign mem)
+                        
+                    ^ to the abv hashmap
+                */
+            }
+
+            sleep(Duration::from_millis(core.driver_poll_rate));
         }
     }
 
--- um_engine/src/core/process_monitor.rs
+++ um_engine/src/core/process_monitor.rs
@@ -1,9 +1,65 @@
-/// The ProcessMonitor is responsible for monitoring all processes running
-/// on the system, and keeping track of risk scores, and activity conducted
-/// by processes.
+use std::collections::HashMap;
+
+use shared_no_std::driver_ipc::ProcessStarted;
+
+/// The ProcessMonitor is responsible for monitoring all processes running; this 
+/// structure holds a hashmap of all processes by the pid as an integer, and 
+/// the data within is a MonitoredProcess containing the details
+#[derive(Debug)]
 pub struct ProcessMonitor {
-    pid: u32,
-    process_name: String,
+    processes: HashMap<u64, Process>
+}
+
+pub enum ProcessErrors {
+    PidNotFound,
+    DuplicatePid,
+}
+
+/// The Process is a structural representation of an individual process thats
+/// running on the host machine, and keeping track of risk scores, and activity conducted
+/// by processes. 
+#[derive(Debug)]
+pub struct Process {
+    pid: u64,
+    process_image: String,
+    commandline_args: String,
     risk_score: u8,
     allow_listed: bool, // whether the application is allowed to exist without monitoring
+}
+
+impl ProcessMonitor {
+    pub fn new() -> Self {
+        ProcessMonitor {
+            processes: HashMap::new(),
+        }
+    }
+
+    /// todo more fn comments
+    pub fn insert(&mut self, proc: &ProcessStarted) -> Result<(), ProcessErrors> {
+        //
+        // First check we aren't inserting a duplicate PID, this may happen if we haven't received
+        // a notification that a process has been terminated; or that we have a new process queued to
+        // insert before a delete item which is queued.
+        // todo this can be solved by first batch running deletes, before running updates.
+        //
+
+        let entry = self.processes.get(&proc.pid);
+        if entry.is_some() {
+            return Err(ProcessErrors::DuplicatePid);
+        }
+
+        self.processes.insert(proc.pid, Process {
+            pid: proc.pid,
+            process_image: proc.image_name.clone(),
+            commandline_args: proc.command_line.clone(),
+            risk_score: 0,
+            allow_listed: false,
+        });
+
+        Ok(())
+    }
+
+    pub fn remove_process(&mut self, pid: u64) {
+        self.processes.remove(&pid);
+    }
 }
